419ad2ad95516dc21ca2ee6995f7a184857c8e0d
Nurlybek <nurlybekqwerty@gmail.com>
2023-11-17 15:13:08 +0600

    add vuetify first component layout

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,37 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+
+const order=ref(0)
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+ <v-layout class="rounded rounded-md">
+    <v-navigation-drawer color="grey-darken-2" permanent></v-navigation-drawer>
 
-    <div class="wrapper">
-      <v-app-bar >
-        
-      </v-app-bar>
+    <v-app-bar
+      :order="order"
+      color="grey-lighten-2"
+      flat
+      title="Application bar"
+    >
+      <template v-slot:append>
+        <v-switch
+          v-model="order"
+          hide-details
+          inset
+          label="Toggle order"
+          true-value="-1"
+          false-value="0"
+        ></v-switch>
+      </template>
+    </v-app-bar>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/test">Test</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+    <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+      Main Content
+    </v-main>
+  </v-layout>
 </template>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 import './assets/main.css'
-
+import App from './App.vue'
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import router from './router'
+// Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-
-import App from './App.vue'
-import router from './router'
+import { createPinia } from 'pinia'
 
 const app = createApp(App)
 
@@ -20,6 +19,4 @@ const vuetify = createVuetify({
 
 app.use(createPinia())
 app.use(router)
-app.use(vuetify)
-
-app.mount('#app')
+app.use(vuetify).mount('#app')
