b04dc5f6fe6dc852514916ba7a051526cc15a3db
Nurlybek <nurlybekqwerty@gmail.com>
2024-01-15 17:29:47 +0600

    Add tailwind config

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
-
-
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-import { ref,watchEffect } from 'vue'
+import "./main.css";
+import { ref, watchEffect } from 'vue';
 interface TodoItem{
   userId: number,
   id: number,
@@ -13,44 +10,20 @@ interface TodoItem{
 const drawer=ref(false);
 const commits = ref<TodoItem[]|null>(null);
 
+defineProps({
+  greeting:String
+})
+
 watchEffect(async()=>{
   commits.value=await (await fetch('https://jsonplaceholder.typicode.com/todos')).json();
 })
-console.log(commits)
 </script>
 
 <template>
-  <v-app id="inspire">
-    <v-navigation-drawer v-model="drawer">
-      <!--  -->
-    </v-navigation-drawer>
-
-    <v-app-bar>
-      <v-app-bar-nav-icon @click="drawer = !drawer">
-        <template v-slot:append>
-          <v-btn icon="mdi-heart"></v-btn>
-
-          <v-btn icon="mdi-magnify"></v-btn>
-
-          <v-btn icon="mdi-dots-vertical"></v-btn>
-        </template>
-      </v-app-bar-nav-icon>
-
-      <v-app-bar-title>Application</v-app-bar-title>
-    </v-app-bar>
-
-    <v-main>
-      <v-list>
-        <v-list-item
-        v-for="n in commits"
-        :key="n.id"
-        :title="n.title"
-        subtitle="Lorem ipsum dolor sit amet consectetur adipisicing elit"
-        >
-        </v-list-item>
-      </v-list>
-    </v-main>
-  </v-app>
+  <div class="flex">
+    123
+  </div>
+ 
 </template>
 
 <style scoped>
