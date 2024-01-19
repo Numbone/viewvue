8455cb5b238852599aca666f8d5799fd8ebf839c
Nurlybek <nurlybekqwerty@gmail.com>
2024-01-19 15:43:46 +0600

    card component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
 import './main.css'
 import HeaderComponent from '@/components/HeaderComponent.vue'
+import CardComponent from '@/components/CardComponent.vue'
 </script>
 
 <template>
-  <div class="bg-white w-4/5 m-auto h-screen rounded-xl mt-14">
+  <div class="bg-white w-4/5 m-auto rounded-xl mt-14">
     <HeaderComponent />
+    <div>
+      <CardComponent />
+      <CardComponent />
+      <CardComponent />
+      <CardComponent />
+      <CardComponent />
+      <CardComponent />
+      <CardComponent />
+      
+    </div>
   </div>
 </template>
 
---
+++ src/components/CardComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    class="relative m-10 border border-slate-100 rounded-3xl p-8 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl"
+  >
+    <img class="absolute top-8 left-8" :src="Liked" alt="liked" />
+    <img :src="Sneakers" alt="sneakers" />
+    <p class="pt-2">Nike Blazer Mid Suede</p>
+
+    <div class="flex justify-between mt-5">
+      <div class="flex flex-col">
+        <span class="text-slate-400">Price:</span>
+        <span>1222</span>
+      </div>
+
+      <img :src="Plus" alt="" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Liked from '@/assets/icons/like-1.svg'
+import Like from '@/assets/icons/like-2.svg'
+import Sneakers from '@/assets/images/sneakers-1.jpg'
+import Plus from '@/assets/icons/plus.svg'
+</script>
+
+<style scoped></style>
